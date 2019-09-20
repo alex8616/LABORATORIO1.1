632e3f077fe6cd36d6e86edd136285e668240c21
selena <quintanillafuertesselena@gmail.com>
2019-09-20 00:29:53 -0400

    Prefix

---
+++ solutions/longestDigitsPrefix.js
@@ -0,0 +1,24 @@
+function longestDigitsPrefix(str1)
+{
+	l = str1.length;
+	var fin =1;
+	var r = null;
+	entero = 0;
+	for (null;fin<=l;fin++)
+	{
+		try
+		{
+			r = parseInt(str1.substring(0,fin));
+		}
+		catch
+		{
+			fin--;
+			r parseInt(str1.substring(0,fin));
+			entero = 1;
+			break;
+		}
+
+	}
+	return r
+}
+console.log(longestDigitsPrefix("1348dasd sds"));
